608767e810be0c4f8308bec33fccad09d88c7970
PP <chinese1236@@.com>
2019-11-05 11:14:18 +0800

    support cocos services

--- platforms/wechat/res/game.js
+++ platforms/wechat/res/game.js
@@ -9,6 +9,7 @@ window.__globalAdapter.init(function () {
     // Will be replaced with cocos2d-js path in editor
     require('cocos2d-js-path');
     require('./libs/common/engine/index');
+    // Introduce Cocos Services here
     require('./main');
     require('./libs/common/remote-downloader');
 
