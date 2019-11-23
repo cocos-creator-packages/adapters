e6b98446f1ba89088ac7f288bc759d13f5b11947
ChiaNing <sun.cloud.mo@gmail.com>
2019-11-23 14:12:51 +0800

    3D adapters

---
+++ common/engine3d/Texture2D.js
@@ -0,0 +1,14 @@
+// 无此方法
+
+const Texture2D = cc.Texture2D;
+
+if (Texture2D) {
+    Object.assign(Texture2D.prototype, {
+        initWithElement (element) {
+            if (!element)
+                return;
+            this._image = element;
+            this.handleLoadedTexture();
+        },
+    });
+}
---
+++ common/engine3d/index.js
@@ -0,0 +1,10 @@
+require('./Audio');
+require('./AudioEngine');
+require('./DeviceMotionEvent');
+require('./Editbox');
+require('./Game');
+require('./InputManager');
+require('./Loader');
+require('./Screen');
+require('./Texture2D');
+require('./VideoPlayer');
