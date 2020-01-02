6c0144dd2b71c3093a4c4818e5725ec2e69226ee
PP <chinese1236@@.com>
2020-01-02 15:34:35 +0800

    sync baidu applySettings

--- platforms/wechat/wrapper/sub-context-adapter.js
+++ platforms/wechat/wrapper/sub-context-adapter.js
@@ -74,26 +74,9 @@ cc.Canvas.prototype.update = function () {
         this.applySettings();
     }
 };
-
+let originApplySettings = cc.Canvas.prototype.applySettings;
 cc.Canvas.prototype.applySettings = function () {
-    var ResolutionPolicy = cc.ResolutionPolicy;
-    var policy;
-    if (this.fitHeight && this.fitWidth) {
-        policy = ResolutionPolicy.SHOW_ALL;
-    }
-    else {
-        if (this.fitWidth) {
-            policy = ResolutionPolicy.FIXED_WIDTH;
-        }
-        else if (this.fitHeight) {
-            policy = ResolutionPolicy.FIXED_HEIGHT;
-        }
-        else {
-            policy = ResolutionPolicy.NO_BORDER
-        }
-    }
-    var designRes = this._designResolution;
-    cc.view.setDesignResolutionSize(designRes.width, designRes.height, policy);
+    originApplySettings.call(this);
     this._width = cc.game.canvas.width;
     this._height = cc.game.canvas.height;
 };
