84d45bbb90324882cdab362bc068071f7e5c2bbe
ChiaNing <sun.cloud.mo@gmail.com>
2020-03-24 19:43:06 +0800

    function remove

--- platforms/baidu/wrapper/unify.js
+++ platforms/baidu/wrapper/unify.js
@@ -35,21 +35,6 @@ if (window.__globalAdapter) {
 
     // Subpackage
     // baidu can`t use system So need special treatment
-    cc.loader.downloader.loadSubpackage = function(name, completeCallback) {
-        swan.loadSubpackage({
-            name: name,
-            success: function() {
-                cc$System.import('virtual:///prerequisite-imports/' + name).then(function() {
-                    if (completeCallback) { completeCallback(); }
-                }).catch(function(err) {
-                    if (completeCallback) { completeCallback(err); }
-                });
-            },
-            fail: function() {
-                if (completeCallback) { completeCallback(new Error(`Failed to load subpackage ${name}`)); }
-            },
-        });
-    };
     // utils.cloneMethod(globalAdapter, swan, 'loadSubpackage');
 
     // SharedCanvas
