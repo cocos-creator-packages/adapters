17075e467a8dbb5fbd9710f18244cb02b56005cb
ChiaNing <sun.cloud.mo@gmail.com>
2020-03-25 10:16:51 +0800

    format fix

--- platforms/baidu/wrapper/engine/Loader.js
+++ platforms/baidu/wrapper/engine/Loader.js
@@ -1,16 +1,16 @@
-    // baidu can`t use system So need special treatment
-    cc.loader.downloader.loadSubpackage = function(name, completeCallback) {
-        swan.loadSubpackage({
-            name: name,
-            success: function() {
-                cc$System.import('virtual:///prerequisite-imports/' + name).then(function() {
-                    if (completeCallback) { completeCallback(); }
-                }).catch(function(err) {
-                    if (completeCallback) { completeCallback(err); }
-                });
-            },
-            fail: function() {
-                if (completeCallback) { completeCallback(new Error(`Failed to load subpackage ${name}`)); }
-            },
-        });
-    };
+// baidu can`t use system So need special treatment
+cc.loader.downloader.loadSubpackage = function(name, completeCallback) {
+    swan.loadSubpackage({
+        name: name,
+        success: function() {
+            cc$System.import('virtual:///prerequisite-imports/' + name).then(function() {
+                if (completeCallback) { completeCallback(); }
+            }).catch(function(err) {
+                if (completeCallback) { completeCallback(err); }
+            });
+        },
+        fail: function() {
+            if (completeCallback) { completeCallback(new Error(`Failed to load subpackage ${name}`)); }
+        },
+    });
+};
