326a19b3510b104a3444d11ae9ecfbc3cf0e3964
PP_Pro <chinese1236@qq.com>
2021-03-03 14:32:10 +0800

    optimize Label black border on bytedance platform

--- platforms/bytedance/wrapper/engine/Label.js
+++ platforms/bytedance/wrapper/engine/Label.js
@@ -22,4 +22,38 @@ if (cc && cc.Label) {
             });
         });
     }
+
+    // need to fix ttf font black border at the sdk verion lower than 2.0.0
+    let sysInfo = tt.getSystemInfoSync();
+    if (Number.parseInt(sysInfo.SDKVersion[0]) < 2) {
+        let _originUpdateMaterial = Label.prototype._updateMaterialWebgl;
+        Object.assign(Label.prototype, {
+            _updateMaterialWebgl () {
+                _originUpdateMaterial.call(this);
+                // only fix when srcBlendFactor is SRC_ALPHA
+                if (this.srcBlendFactor !== cc.macro.BlendFactor.SRC_ALPHA
+                    || globalAdapter.isDevTool || this.font instanceof cc.BitmapFont) {
+                    return;
+                }
+    
+                // init blend factor
+                let material = this._materials[0];
+                if (!this._frame || !material) {
+                    return;
+                }
+                let dstBlendFactor = this.dstBlendFactor;
+                // Premultiplied alpha on runtime when sdk verion is lower than 2.0.0
+                let srcBlendFactor = cc.macro.BlendFactor.ONE;
+    
+                // set blend func
+                material.effect.setBlend(
+                    true,
+                    gfx.BLEND_FUNC_ADD,
+                    srcBlendFactor, dstBlendFactor,
+                    gfx.BLEND_FUNC_ADD,
+                    srcBlendFactor, dstBlendFactor,
+                );
+            },
+        });
+    }
 }
