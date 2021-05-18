a9f8a050d686b23874400dd2bbdc4ecff5c0b489
PP_Pro <chinese1236@qq.com>
2021-05-18 17:06:00 +0800

    fix $global.screencanvas.getBoundingClientRect

--- platforms/taobao/wrapper/builtin/WindowProperties.js
+++ platforms/taobao/wrapper/builtin/WindowProperties.js
@@ -1,7 +1,15 @@
-const { pixelRatio } = my.getSystemInfoSync()
+const { pixelRatio, screenWidth, screenHeight } = my.getSystemInfoSync()
 const devicePixelRatio = pixelRatio;
 
-let { width, height } = $global.screencanvas.getBoundingClientRect();
+let width, height;
+if ($global.screencanvas.getBoundingClientRect) {
+  let rect = $global.screencanvas.getBoundingClientRect();
+  width = rect.width;
+  height = rect.height;
+} else {
+  width = screenWidth;
+  height = screenHeight;
+}
 export const innerWidth = width;
 export const innerHeight = height;
 export { devicePixelRatio }
