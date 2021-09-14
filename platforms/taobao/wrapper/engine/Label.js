ab14a7586b47dafea8658714119f3e435eb42c5d
PP <chinese1236@qq.com>
2021-09-14 11:14:39 +0800

    share the offscreen canvas (#245)

--- platforms/taobao/wrapper/engine/Label.js
+++ platforms/taobao/wrapper/engine/Label.js
@@ -2,6 +2,25 @@ if (cc && cc.Label) {
     const gfx = cc.gfx;
     const Label = cc.Label;
 
+    // shared label canvas
+    let _sharedLabelCanvas = document.createElement('canvas');
+    let _sharedLabelCanvasCtx = _sharedLabelCanvas.getContext('2d');
+    let canvasData = {
+        canvas: _sharedLabelCanvas,
+        context: _sharedLabelCanvasCtx,
+    };
+    cc.game.on(cc.game.EVENT_ENGINE_INITED, function () {
+        Object.assign(Label._canvasPool, {
+            get() {
+                return canvasData;
+            },
+
+            put() {
+                // do nothing
+            }
+        });
+    });
+
     let _originUpdateMaterial = Label.prototype._updateMaterialWebgl;
     // fix ttf font black border
     Object.assign(Label.prototype, {
