5291c6fee41b23bf81116f28c061d3d714006aa3
PP_Pro <chinese1236@qq.com>
2021-10-23 23:06:18 +0800

    remove image error callback & remove proxy usage

--- platforms/taobao/wrapper/engine/AssetManager.js
+++ platforms/taobao/wrapper/engine/AssetManager.js
@@ -1,11 +1,70 @@
+const screencanvas = $global.screencanvas;
 const parser = cc.assetManager.parser;
 const downloader = cc.assetManager.downloader;
+
+function parseParameters (options, onProgress, onComplete) {
+    if (onComplete === undefined) {
+        var isCallback = typeof options === 'function';
+        if (onProgress) {
+            onComplete = onProgress;
+            if (!isCallback) {
+                onProgress = null;
+            }
+        }
+        else if (onProgress === undefined && isCallback) {
+            onComplete = options;
+            options = null;
+            onProgress = null;
+        }
+        if (onProgress !== undefined && isCallback) {
+            onProgress = options;
+            options = null;
+        }
+    }
+    options = options || Object.create(null);
+    return { options, onProgress, onComplete };
+}
 
 function doNothing (url, options, onComplete) {
     onComplete(null, url);
 }
 
 downloader.downloadDomAudio = doNothing;
+
+function downloadImage (url, options, onComplete) {
+    const parameters = parseParameters(options, undefined, onComplete);
+    options = parameters.options;
+    onComplete = parameters.onComplete;
+
+    const img =  screencanvas.createImage();
+
+    const timer = setTimeout(() => {
+        clearEvent();
+        onComplete && onComplete(new Error(cc.debug.getError(4930, url)));
+    }, 8000);
+    function clearEvent () {
+        img.onload = null;
+        // img.onerror = null;
+    }
+    function loadCallback () {
+        clearEvent();
+        clearTimeout(timer);
+        onComplete && onComplete(null, img);
+    }
+    
+    function errorCallback () {
+        clearEvent();
+        clearTimeout(timer);
+        onComplete && onComplete(new Error(cc.debug.getError(4930, url)));
+    }
+
+    img.onload = loadCallback;
+    // NOTE: crash when registering error callback
+    // img.onerror = errorCallback;
+    img.src = url;
+    return img;
+}
+downloader.downloadDomImage = downloadImage;
 
 downloader.register({
     // Audio
@@ -34,4 +93,15 @@ parser.register({
     '.ogg' : doNothing,
     '.wav' : doNothing,
     '.m4a' : doNothing,
+
+    // Image
+    '.png' : downloadImage,
+    '.jpg' : downloadImage,
+    '.bmp' : downloadImage,
+    '.jpeg' : downloadImage,
+    '.gif' : downloadImage,
+    '.ico' : downloadImage,
+    '.tiff' : downloadImage,
+    '.image' : downloadImage,
+    '.webp' : downloadImage,
 });
