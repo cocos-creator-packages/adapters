9fcb1a5332f0da169a9e3a58ccb25597102c0c42
finscn <finscn@gmail.com>
2022-04-21 14:57:00 +0800

    防止 cc.Canvas.instance 不存在时报错. (#230)
    * 防止 cc.Canvas.instance 不存在时报错.
    
    * Update error-reporter.js

--- platforms/wechat/wrapper/error-reporter.js
+++ platforms/wechat/wrapper/error-reporter.js
@@ -14,7 +14,12 @@ function onErrorMessageHandler (info) {
     if (!env) {
         return;
     }
-    var root = cc.Canvas.instance.node;
+
+    if (!cc.Canvas.instance) {
+        return;
+    }
+
+    var root = cc.Canvas.instance.node
     if (!root) {
         return;
     }
