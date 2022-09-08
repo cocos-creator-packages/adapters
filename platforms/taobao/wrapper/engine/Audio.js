b60fcbe6722a61f880f22f22e8e6bb9dcdfe1b34
PP_Pro <chinese1236@qq.com>
2022-09-08 17:18:46 +0800

    refactor: optimize audio adapter on Taobao

--- platforms/taobao/wrapper/engine/Audio.js
+++ platforms/taobao/wrapper/engine/Audio.js
@@ -1,62 +1,167 @@
-const Audio = cc._Audio;
-const sys = cc.sys;
+const game = cc.game;
+const EventTarget = cc.EventTarget;
 
-const originalPlay = Audio.prototype.play;
-const originalSetCurrentTime = Audio.prototype.setCurrentTime;
-const originalStop = Audio.prototype.stop;
-
-if (Audio) {
-    Object.assign(Audio.prototype, {
-        _currentTime: 0,
-        _hasPlayed: false,
-
-        _createElement () {
-            let url = this._src._nativeAsset;
-            // Reuse dom audio element
-            if (!this._element) {
-                this._element = __globalAdapter.createInnerAudioContext();
-            }
-            this._element.src = url;
-        },
-
-        play () {
-            this._hasPlayed = true;
-            originalPlay.call(this);
-            if (sys.os === sys.OS_ANDROID && this._currentTime !== 0 && this._element) {
-                this._element.seek(this._currentTime);
-                this._currentTime = 0;  // clear currentTime cache
-            }
-        },
-
-        stop () {
-            // HACK: on Android, can't call stop before first playing
-            if (sys.os === sys.OS_ANDROID) {
-                if (!this._hasPlayed){
-                    return;
-                }
-                originalStop.call(this);
-            }
-            // HACK: fix audio seeking on iOS end
-            else {
-                let self = this;
-                this._src && this._src._ensureLoaded(function () {
-                    // should not seek on iOS end
-                    // self._element.seek(0);
-                    self._element.stop();
-                    self._unbindEnded();
-                    self.emit('stop');
-                    self._state = Audio.State.STOPPED;
-                });
-            }
-        },
-
-        setCurrentTime (num) {
-            // HACK: on Android, cannot call seek before playing
-            if (sys.os === sys.OS_ANDROID && this._element && this._element.paused) {
-                this._currentTime = num;
-            } else {
-                originalSetCurrentTime.call(this, num);
-            }
-        },
-    });
+const State = {
+    ERROR: -1,
+    INITIALZING: 0,
+    PLAYING: 1,
+    PAUSED: 2,
+    STOPPED: 3,
 }
+
+function Audio (nativeAudio, loadPromise) {
+    this._nativeAudio = nativeAudio;
+    this._src = '';
+    this._et = new EventTarget();
+    this.reset();
+    loadPromise.then(() => {
+        this._duration = nativeAudio.duration;
+    });
+    this._onShow = () => {
+        if (this._blocked) {
+            this._nativeAudio.play();
+        }
+        this._blocked = false;
+    };
+    this._onHide = () => {
+        if (this.getState() === State.PLAYING) {
+            this._nativeAudio.pause();
+            this._blocked = true;
+        }
+    };
+    // HACK: the onEnded callback strangely executes before playing...
+    setTimeout(() => {
+        nativeAudio.onEnded(() => {
+            this.finishCB && this.finishCB();
+            this._state = State.INITIALZING;
+            this._et.emit('ended');
+        });
+    }, 100);
+    nativeAudio.onStop(() => { this._et.emit('stop'); });
+    nativeAudio.onTimeUpdate(() => { this._currentTime = nativeAudio.currentTime; });
+    game.on(game.EVENT_SHOW, this._onShow);
+    game.on(game.EVENT_HIDE, this._onHide);
+}
+
+Audio.State = State;
+
+Audio.loadNative = function (url) {
+    const nativeAudio = my.createInnerAudioContext();
+    const loadPromise = new Promise((resolve, reject) => {
+        let done = false;
+        // HACK: onCanplay callback not working on Taobao.
+        let timer = setTimeout(() => {
+            if (done) { return; }
+            cc.warn('Timeout to load audio');
+            done = true;
+            resolve(nativeAudio);
+        }, 3000);
+        nativeAudio.onCanplay(() => {
+            if (done) { return; }
+            clearTimeout(timer);
+            done = true;
+            resolve(nativeAudio);
+        });
+        nativeAudio.onError((err) => {
+            if (done) { return; }
+            clearTimeout(timer);
+            done = true;
+            reject(err);
+        });
+        nativeAudio.src = url;
+    });
+    return { nativeAudio, loadPromise };
+};
+
+Audio.load = function (url) {
+    const { nativeAudio, loadPromise } = Audio.loadNative(url);
+    const audio = new Audio(nativeAudio, loadPromise);
+    return { audio, loadPromise };
+};
+
+Object.assign(Audio.prototype, {
+
+    reset () {
+        this.id = -1;
+        this.finishCB = null;  // For audioEngine custom ended callback.
+        this._state = State.INITIALZING;
+        this._loop = false;
+        this._currentTime = 0;
+        this._volume = 1;
+        this._blocked = false;
+    },
+
+    destroy () {
+        game.off(game.EVENT_SHOW, this._onShow);
+        game.off(game.EVENT_HIDE, this._onHide);
+        // offStop offEnded is not supported for now.
+        this._nativeAudio.destroy();
+        this._nativeAudio = null;
+    },
+
+    getSrc () { return this._src; },
+    setSrc (path) { 
+        if (this._src === path) {
+            return;
+        }
+        this._nativeAudio.src = path;
+        this._src = path;
+    },
+    getState () { return this._state; },
+    getDuration () { return this._duration; },
+    // getCurrentTime () { return this._currentTime; },  // onTimeUpdate not working...
+    getCurrentTime () { return this._nativeAudio.currentTime; },
+    seek (val) {
+        if (this._currentTime === val) {
+            return;
+        }
+        this._nativeAudio.seek(val);
+        this._currentTime = val;
+    },
+    getLoop () { return this._loop; },
+    setLoop (val) {
+        if (this._loop === val) {
+            return;
+        }
+        this.nativeAudio.loop = val;
+        this._loop = val;
+    },
+    getVolume () { return this._volume; },
+    setVolume (val) {
+        if (this._volume === val) {
+            return;
+        }
+        this.nativeAudio.volume = val;
+        this._volume = val;
+    },
+
+    play () {
+        if (this.getState() !== State.PLAYING) {
+            this._nativeAudio.play();
+            this._state = State.PLAYING;
+        }
+    },
+    resume () {
+        if (this.getState() === State.PAUSED) {
+            this._nativeAudio.play();
+            this._state = State.PLAYING;
+        }
+    },
+    pause () {
+        if (this.getState() === State.PLAYING) {
+            this._nativeAudio.pause();
+            this._state = State.PAUSED;
+        }
+    },
+    stop () {
+        this._nativeAudio.stop();
+        this._state = State.STOPPED;
+    },
+
+    onEnded (cb) { this._et.on('ended', cb); },
+    offEnded (cb = undefined) { this._et.off('ended', cb); },
+    onStop (cb) { this._et.on('stop', cb); },
+    offStop (cb = undefined) { this._et.off('stop', cb); },
+});
+
+module.exports = Audio;
---
+++ platforms/taobao/wrapper/engine/AudioEngine.js
@@ -0,0 +1,409 @@
+const Audio = require('./Audio');
+const AudioClip = cc.AudioClip;
+const js = cc.js;
+
+let _instanceId = 0;
+const _id2audio = {};
+const _audioPool = [];
+const _maxPoolSize = 32;
+
+function getOrCreateAudio (path) {
+    const audio = _audioPool.pop();
+    if (audio) {
+        audio.id = ++_instanceId;
+        return { audio, loadPromise: Promise.resolve(), };
+    }
+    const { audio: loadedAudio, loadPromise } = Audio.load(path);
+    loadedAudio.id = ++_instanceId;
+    return { audio: loadedAudio, loadPromise };
+}
+
+function putOrDestroyAudio (audio) {
+    if (_audioPool.includes(audio)) {
+        return;
+    }
+    delete _id2audio[audio.id];
+    if (_audioPool.length < _maxPoolSize) {
+        _audioPool.push(audio);
+    } else {
+        audio.destroy();
+    }
+}
+
+const _playingAudio = [];
+const _maxPlayingAudio = 10;
+
+function addPlaying (audio) {
+    if (!_playingAudio.includes(audio)) {
+        _playingAudio.push(audio);
+    }
+}
+
+function removePlaying (audio) {
+    const index = _playingAudio.indexOf(audio);
+    if (index !== -1) {
+        js.array.fastRemoveAt(_playingAudio, index);
+    }
+}
+
+function stopAudioIfNeccessary () {
+    if (_playingAudio.length >= _maxPlayingAudio) {
+        const oldestAudio = _playingAudio.shift();
+        if (oldestAudio.getState() === Audio.State.PLAYING) {
+            oldestAudio.stop();
+        }
+        removePlaying(oldestAudio);
+        putOrDestroyAudio(oldestAudio);
+    }
+}
+
+
+cc.audioEngine = {
+
+    AudioState: Audio.State,
+
+    _maxPoolSize: 32,
+
+    _id2audio,
+
+    _pauseIDCache: [],
+
+    _play (clip, loop, volume) {
+        if (!(clip instanceof AudioClip)) {
+            return cc.error('Wrong type of AudioClip.');
+        }
+        let path = clip.nativeUrl;
+        let { audio, loadPromise } = getOrCreateAudio(path);
+
+        loadPromise.then(() => {
+            audio.setSrc(path);
+            audio.setLoop(loop || false);
+            audio.setVolume(volume);
+    
+            stopAudioIfNeccessary();
+            audio.play();
+            addPlaying(audio);
+        });
+
+        return audio;
+    },
+
+    play: function (clip, loop, volume) {
+        const audio = this._play(clip, loop, volume);
+        this._id2audio[audio.id] = audio;
+        return audio.id;
+    },
+
+    setLoop: function (id, loop) {
+        const audio = this._id2audio[id];
+        if (audio) {
+            audio.setLoop(loop);
+        }
+    },
+
+    isLoop: function (id) {
+        const audio = this._id2audio[id];
+        if (audio) {
+            return audio.getLoop();
+        }
+        return false;        
+    },
+
+    setVolume: function (id, volume) {
+        const audio = this._id2audio[id];
+        if (audio) {
+            return audio.setVolume(volume);
+        }
+    },
+
+    getVolume: function (id) {
+        const audio = this._id2audio[id];
+        if (audio) {
+            return audio.getVolume();
+        }
+        return 1;        
+    },
+    
+    setCurrentTime: function (id, sec) {
+        const audio = this._id2audio[id];
+        if (audio) {
+            return audio.seek(sec);
+        }
+    },
+
+    
+    getCurrentTime: function (id) {
+        const audio = this._id2audio[id];
+        if (audio) {
+            return audio.getCurrentTime();
+        }
+        return 0;
+    },
+    
+    getDuration: function (id) {
+        const audio = this._id2audio[id];
+        if (audio) {
+            return audio.getDuration();
+        }
+        return 1;        
+    },
+
+    
+    getState: function (id) {
+        const audio = this._id2audio[id];
+        if (audio) {
+            return audio.getState();
+        }
+        return Audio.State.INITIALZING;
+        
+    },
+
+    
+    isPlaying: function(id) {
+        const audio = this._id2audio[id];
+        if (audio) {
+            return audio.getState() === Audio.State.PLAYING;
+        }
+        return false;        
+    },
+
+    setFinishCallback: function (id, callback) {
+        const audio = this._id2audio[id];
+        if (audio) {
+            return audio.finishCB = callback;
+        }        
+    },
+
+    
+    pause: function (id) {
+        const audio = this._id2audio[id];
+        if (audio) {
+            audio.pause();
+        }        
+    },
+
+    
+    pauseAll: function () {
+        for (let id in this._id2audio) {
+            const audio = this._id2audio[id];
+            if (audio) {
+                audio.pause();
+            }
+        }
+    },
+
+    resume: function (id) {
+        const audio = this._id2audio[id];
+        if (audio) {
+            audio.resume();
+        }    
+    },
+
+    resumeAll: function () {
+        for (let id in this._id2audio) {
+            const audio = this._id2audio[id];
+            if (audio) {
+                audio.resume();
+            }
+        }     
+    },
+
+    
+    stop: function (id) {
+        const audio = this._id2audio[id];
+        if (audio) {
+            audio.stop();
+        }        
+    },
+
+    
+    stopAll: function () {
+        for (let id in this._id2audio) {
+            const audio = this._id2audio[id];
+            if (audio) {
+                audio.stop();
+            }
+        }      
+    },
+
+    
+    setMaxAudioInstance: function (num) {
+        // NOT SUPPPORTED
+    },
+
+    getMaxAudioInstance: function () {
+        return _maxPlayingAudio;
+    },
+    
+    uncache: function (clip) {
+        var filePath = clip;
+        if (typeof clip === 'string') {
+            // backward compatibility since 1.10
+            cc.warnID(8401, 'cc.audioEngine', 'cc.AudioClip', 'AudioClip', 'cc.AudioClip', 'audio');
+            filePath = clip;
+        }
+        else {
+            if (!clip) {
+                return;
+            }
+            filePath = clip.nativeUrl;
+        }
+
+        for (let id in _id2audio) {
+            const audio = this._id2audio[id];
+            if (audio && audio.getSrc() === filePath) {
+                audio.stop();
+                putOrDestroyAudio(audio);
+            }
+        }
+    },
+
+    uncacheAll: function () {
+        this.stopAll();
+        for (let id in _id2audio) {
+            const audio = _id2audio[id];
+            if (audio) {
+                audio.stop();
+                putOrDestroyAudio(audio);
+            }
+        }
+    },
+
+    _break: function () {
+        // DO NOTHING
+    },
+
+    _restore: function () {
+        // DO NOTHING        
+    },
+
+    ///////////////////////////////
+    // Classification of interface
+
+    _music: null,
+
+    _effect: {
+        audios: [],
+        volume: 1,
+    },
+
+    
+    playMusic: function (clip, loop) {
+        if (this._music.getSrc() !== clip.nativeUrl) {
+            this._music.stop();
+            removePlaying(this._music);
+            putOrDestroyAudio(this._music);
+        }
+        const audio = this._play(clip, loop);
+        this._music = audio;
+        return audio.id;
+    },
+
+    
+    stopMusic: function () {
+        this._music.stop();
+        removePlaying(this._music);
+    },
+
+    
+    pauseMusic: function () {
+        this._music.pause();
+        removePlaying(this._music);        
+    },
+
+    
+    resumeMusic: function () {
+        this._music.resume();
+        addPlaying(this._music);     
+    },
+    
+    getMusicVolume: function () {
+        if (this._music) {
+            return this._music.getVolume();
+        }
+        return 1;
+    },
+    
+    setMusicVolume: function (volume) {
+        if (this._music) {
+            this._music.setVolume(volume);
+        }
+    },
+    
+    isMusicPlaying: function () {
+        return this._music.getState() === Audio.State.PLAYING;
+    },
+    
+    playEffect: function (clip, loop) {
+        const audio = this._play(clip, loop, this._effect.volume);
+        this._effect.audios.push(audio);
+        addPlaying(audio);
+        return audio.id;
+    },
+    
+    setEffectsVolume: function (volume) {
+        this._effect.audios.forEach(audio => {
+            audio.setVolume(volume);
+        });
+        this._effect.volume = volume;
+    },
+    
+    getEffectsVolume: function () {
+        return this._effect.volume;
+    },
+    
+    pauseEffect: function (id) {
+        this._effect.audios.some(audio => {
+            if (audio.id === id) {
+                audio.pause();
+                removePlaying(audio);
+                return true;
+            }
+            return false;
+        });
+    },
+    
+    pauseAllEffects: function () {
+        this._effect.audios.forEach(audio => {
+            audio.pause();
+            removePlaying(audio);
+        });        
+    },
+    
+    resumeEffect: function (id) {
+        this._effect.audios.some(audio => {
+            if (audio.id === id) {
+                audio.resume();
+                addPlaying(audio);
+                return true;
+            }
+            return false;
+        });        
+    },
+    
+    resumeAllEffects: function () {
+        this._effect.audios.forEach(audio => {
+            audio.resume();
+            addPlaying(audio);
+        });        
+    },
+    
+    stopEffect: function (id) {
+        this._effect.audios.some(audio => {
+            if (audio.id === id) {
+                audio.stop();
+                removePlaying(audio);
+                return true;
+            }
+            return false;
+        });        
+    },
+    
+    stopAllEffects: function () {
+        this._effect.audios.forEach(audio => {
+            audio.stop();
+            removePlaying(audio);
+        });        
+    }
+};
+ 
